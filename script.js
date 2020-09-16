9b502a04863a6d43aba9d8f5813f04a628a765f7
Joakim Teixeira <setokim1@hotmail.com>
2020-09-16 12:56:23 -0300

    Add snake initial position

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
 const box = 32
+const snake = []
+
+// Initial coordinates
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 const createBackground = () => {
   context.fillStyle = 'lightgreen'
@@ -9,4 +16,12 @@ const createBackground = () => {
   context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+const createSnake = () => {
+  snake.map((snake) => {
+    context.fillStyle = 'green'
+    context.fillRect(snake.x, snake.y, box, box)
+  })
+}
+
 createBackground()
+createSnake()
